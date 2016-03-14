442b329064cbad84d0c4721c2dd9c1c38d178108
ADrunkRussian <adrunkrussian16@gmail.com>
2016-03-14 19:34:15 -0400

    Update pubsearch.py

--- pubsearch.py
+++ pubsearch.py
@@ -5,6 +5,9 @@
 #Contact me on twitter @ADrunkRussian1
 # V1.2
 
+#Basically made the raw_input fields look a bit better. Some spacing down in the os.system for "theharvester" commands were a bit awkward.
+#The triple quotes I took out except for down in one of them, because I didn't want to reformat it all since it works for now.
+
 import os
 import urllib2
 import json
@@ -24,9 +27,7 @@ while ans:
 	print("2. Metagoofil- Tool for extraction of metadata from public documents belonging to a target company\n")	
 	#print("3. MSFConsole Search_email_Collector - Collects Emails from Google, Bing and PGP")
 	print("8. Download or update modules\n")
-	ans=raw_input(
-"""Which tool would you like to use? (Select Your tool)
-""")
+	ans=raw_input("Which tool would you like to use? (Select Your tool): ")
 	
 	if ans=="0":
 		os.system('cls' if os.name == 'nt' else 'clear')
@@ -38,27 +39,31 @@ while ans:
 				print '=============================================='
 				print '#               The Harvester                #'
 				print '=============================================='
-				domain=raw_input("""What is your domain? (i.e google.com)
-"""
-)
+				print
+				domain=raw_input("What is your domain? (i.e google.com)")
+
 
 				os.system('cls' if os.name == 'nt' else 'clear')
-				engine=raw_input("""What data source would you like to use? (google, googlecse, bing, bingapi, pgp, linkedin, google-profiles, people123, jigsaw, twitter, googleplus, all)
-""")
+				
+				engine=raw_input("What data source would you like to use? (google, googlecse, bing, bingapi, pgp, linkedin, google-profiles, people123, jigsaw, twitter, googleplus, all)")
+				
+				
 				os.system('cls' if os.name == 'nt' else 'clear')
-				limit=raw_input("""Limit the number of results to work with: (enter a number)
-""")
-				os.system("sudo theharvester" + " -d " + domain+ " -l " + limit+ " -b " +engine)
+				
+				limit=raw_input("Limit the number of results to work with (enter a number): ")
+				os.system("sudo theharvester" + " -d " + domain + " -l " + limit+ " -b " + engine)
 			
-				print """
-
-If you do not see any results above, The Harvester did not find anything. Don\'t give up, there are other methods."""
+				print ("If you do not see any results above, The Harvester did not find anything. Don\'t give up, there are other methods.")
 			
 
 				break
+				
 			except:
-				print 'You do not The Harvester installed.'
+				print 'You do not have The Harvester installed.'
 				break 	
+				
+				
+				
 #This is the beginning for Metagoofil
 	if ans=="2":
 		os.system('cls' if os.name == 'nt' else 'clear')
@@ -73,13 +78,15 @@ If you do not see any results above, The Harvester did not find anything. Don\'t
 			limit="200"
 			MDomain=raw_input("What is the website you would like to search?\n")
 			filetype=raw_input("What is the filetype you would like to download? (pdf, doc, xls, ppt, odp, ods, docx, xlsx, pptx)\n")
-			limit=raw_input("To how many searches would you like to limit your search to? (Default 200)\n")
+			limit=raw_input("To how many searches would you like to limit yourself to? (Default 200)\n")
 			dllim=raw_input("Limit of files to download\n")
 			wdir=raw_input("What would you like your working directory to be?\n")
 			outf=raw_input("Would you like to output a file? (Y/N)\n")
 			if outf=="Y":
 					outf="-f"
 			os.system("sudo metagoofil " + "-d "+ MDomain + " -t " + filetype + " -l "+ limit + " -n "+ dllim + " -o " + wdir + " -f " + outf) 
+
+
 
 #This is beginning for updates and installation	
 	
@@ -104,11 +111,11 @@ If you do not see any results above, The Harvester did not find anything. Don\'t
 	-s Possible Subdomains
 	-e Possible Email addresses
 	-t Set TTL in seconds for port scanning (Default 2)
-
-Seperate all options with a space. (I.e -i -n -p)
-""")
+Seperate all options with a space. (i.e -i -n -p): """)
 			os.system("sudo dmitry "  + options + " " + ddomain)
 			break
+			
+
 
 	if ans=="8":
 		os.system('cls' if os.name == 'nt' else 'clear')
@@ -123,11 +130,14 @@ Seperate all options with a space. (I.e -i -n -p)
 			print '#                   Emails                   #'	
 			print '=============================================='
 			print '0. The Harvester'
+			print
 			print '1. Dmitry'
+			print
 			print '2. Metagoofil'
+			print
 			#print '2. Metasploit'
 			print '9. Return to Main Menu'
-			upgrade=raw_input("What would you like to update or download?\n")
+			upgrade=raw_input("What would you like to update or download?: \n")
 #Updates The Harvester
 			if upgrade=="0":
 					os.system("sudo apt-get install theharvester")
@@ -151,4 +161,5 @@ Seperate all options with a space. (I.e -i -n -p)
 			#if upgrade=="2":
 					#os.system('cls' if os.name == 'nt' else 'clear')
 					#print '\n\n\n\nWe currently are not supporting Metasploit installations. Sorry'
-				
+					
+					#http://github.com/fawkes-picks/ is a dope ass place to check out.
